9d4a4e89c20f6d31de8c25c5d7445477cc55bfdc
Anindro Bhattacharya <69637288+AB20CS@users.noreply.github.com>
2022-01-18 23:12:54 -0500

    Added --user features

--- mySystemStats.c
+++ mySystemStats.c
@@ -2,16 +2,31 @@
 #include <string.h>
 #include <sys/resource.h>
 #include <sys/utsname.h>
+#include <utmp.h>
 #include <unistd.h>
 
 void generateSystemUsage() {
     printf("---------------------------------------\n");
     printf("Number of cores: %ld\n", sysconf(_SC_NPROCESSORS_ONLN)); // display number of cores
-    
+
 }
 
 void generateUserUsage() {
-    printf("User\n");
+    printf("---------------------------------------\n");
+    printf("### Sessions/users ### \n");
+
+    struct utmp *ut;
+
+    setutent(); // Go to start of user information
+    ut = getutent(); // Reads line from current position (i.e., user information)
+
+    while (ut) {
+        if (ut -> ut_type == USER_PROCESS) {
+            printf("%s\t%s (%s)\n", ut -> ut_user, ut -> ut_line, ut -> ut_host); // displays user login name, device name, and host name
+        }
+        ut = getutent(); // Reads next line
+    }
+
 }
 
 void displaySystemInfo() {
@@ -30,13 +45,16 @@ void displaySystemInfo() {
 int main(int argc, char **argv) {
     
 
-    //int samples;
-    //int tdelay;
+    int samples = 10; // default number of samples set to 10
+    int tdelay = 1; // default number of seconds set to 1
+    
     // Checking if samples is indicated by user
     
 
     // Checking if tdelay is indicated by user
 
+
+    printf("Nbr of samples: %d -- every %d secs\n", samples, tdelay); // Display number of samples and delay
 
     // Choosing type of output
     if (strcmp(argv[1], "--system") == 0) {
